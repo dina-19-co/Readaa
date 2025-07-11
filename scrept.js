f0ba7306680293d768ef5aa924cb26272c504887
dina-19-co <higjfc57@gmail.com>
2025-07-11 21:34:20 +0300

    Add files via upload

---
+++ scrept.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// You can add more JavaScript functionality here if needed in the future.
+// For example, a "scroll to top" button, form validation, animations, etc.
